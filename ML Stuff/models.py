7dadf120cf09901ce193246771aa4150df6f79de
Matthew J Chen <66033424+M-Chen-3@users.noreply.github.com>
2024-05-03 11:46:36 -0400

    Add files via upload

--- ML Stuff/models.py
+++ ML Stuff/models.py
@@ -7,6 +7,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn import svm
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score
 
 TEST_SIZE = 0.2
@@ -19,11 +22,12 @@ def load_data():
     data_list = [pd.DataFrame(), pd.DataFrame()]
 
     for file in data_files:
+        print(f"Loading {file}...")
         df = pd.read_csv(direct + file)
 
-        # Get the labels
+        # Get the labels and remove unwanted ones
         data_list[1] = pd.concat([data_list[1], df["website"]], axis=0)
-        df = df.drop(["website", "time", "description"], axis=1)
+        df = df.drop(["source", "dest", "website", "time", "description"], axis=1)
 
         ### Get the features ###
         # Standardize size column
@@ -35,7 +39,7 @@ def load_data():
         df = pd.concat([size_df, df], axis=1)
 
         # Select the columns we want to one-hot encode (as they aren't numeric)
-        cat_columns = ["source", "dest", "protocol"]
+        cat_columns = ["protocol"]
 
         # One-hot encode those columns and remove old ones
         encoder = OneHotEncoder(sparse_output=False)
@@ -61,6 +65,7 @@ def load_data():
 # Load in the data and replace NaNs with 0
 # NaNs appear when a value in one file doesn't exist in the other file
 x, y = load_data()
+print()
 length = y.shape[0]
 x = x.fillna(0)
 
@@ -73,24 +78,29 @@ x_train, x_valid, y_train, y_valid = train_test_split(
     x_train, y_train, test_size=(1 - TEST_SIZE)*VAL_SIZE
 )
 
-support_vector_machine = svm.SVC(kernel="poly", gamma="scale")
-support_vector_machine.fit(x_train, y_train)
+def test_model(model, name):
+    print(f"Running {name}...")
+    model.fit(x_train, y_train.values.ravel())
 
-svm_valid = support_vector_machine.predict(x_valid)
-svm_test = support_vector_machine.predict(x_test)
+    model_valid = model.predict(x_valid)
+    model_test = model.predict(x_test)
 
-print(f"SVM Accuracy of Validation Set Predictions: {accuracy_score(y_valid, svm_valid)}")
-print(f"SVM F1 Score of Validation Set Predictions: {f1_score(y_valid, svm_valid, average='weighted')}")
-print(f"SVM Accuracy of Testing Set Predictions: {accuracy_score(y_test, svm_test)}")
-print(f"SVM F1 Score of Validation Set Predictions: {f1_score(y_valid, svm_valid, average='weighted')}\n")
+    print(f"{name} Accuracy of Validation Set Predictions: {accuracy_score(y_valid, model_valid)}")
+    print(f"{name} F1 Score of Validation Set Predictions: {f1_score(y_valid, model_valid, average='weighted')}")
+    print(f"{name} Accuracy of Testing Set Predictions: {accuracy_score(y_test, model_test)}")
+    print(f"{name} F1 Score of Testing Set Predictions: {f1_score(y_test, model_test, average='weighted')}\n")
 
 linear_svm = svm.LinearSVC(dual="auto")
-linear_svm.fit(x_train, y_train)
+test_model(linear_svm, "Linear SVM")
 
-linear_valid = linear_svm.predict(x_valid)
-linear_test = linear_svm.predict(x_test)
+support_vector_machine = svm.SVC(kernel="rbf", degree=2, gamma="scale", max_iter=10)
+test_model(support_vector_machine, "SVM")
 
-print(f"Linear Accuracy of Validation Set Predictions: {accuracy_score(y_valid, linear_valid)}")
-print(f"Linear F1 Score of Validation Set Predictions: {f1_score(y_valid, linear_valid, average='weighted')}")
-print(f"Linear Accuracy of Testing Set Predictions: {accuracy_score(y_test, linear_test)}")
-print(f"Linear F1 Score of Validation Set Predictions: {f1_score(y_valid, linear_valid, average='weighted')}")
+multi_nb = MultinomialNB()
+test_model(multi_nb, "Multinomial Naive Bayes")
+
+decision_tree = DecisionTreeClassifier(random_state=0)
+test_model(decision_tree, "Decision Tree")
+
+random_forest = RandomForestClassifier(n_estimators=100)
+test_model(random_forest, "Random Forest")
